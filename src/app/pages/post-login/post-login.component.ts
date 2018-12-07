c1b23f88a828329904b7ee4d716b49fb183c3a5b
Rory Webber <Rory.Webber@arup.com>
2018-12-07 17:20:22 +0000

    #4 tried to make the post-login page redirect to the home page, not currently working.

---
+++ src/app/pages/post-login/post-login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../auth/auth.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-post-login',
+  template: `<p>
+                If you are not redirected soon, please refresh the page click this button
+                </p>
+                <button (click)=click()>click</button>`,
+  styleUrls: ['./post-login.component.less']
+})
+
+export class PostLoginComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { }
+  
+
+  ngOnInit() {
+    console.log("back");
+    
+    
+    
+    // let checkLoggedIn;
+    // checkLoggedIn = setInterval(this.check,1000);
+  }
+  check()
+  {
+    console.log("checking");
+    console.log(this.authService);
+    
+    if (this.authService.isLoggedIn)
+    {
+      // clearInterval(timer);
+      this.router.navigate(['/choose-desk']);
+    }
+    else
+    {
+      console.log("not logged in");
+    }
+  }
+  click()
+  {
+    console.log("clicked");
+    this.router.navigate(['/choose-desk']);
+  }
+
+}
---
+++ src/app/pages/splash/splash.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.component.html',
+  styleUrls: ['./splash.component.less']
+})
+export class SplashComponent implements OnInit {
+
+  constructor(
+    public authService: AuthService,
+    private router: Router
+    ) {}
+
+  ngOnInit()
+  {
+    console.log("Run login");
+    if (!this.authService.isLoggedIn)//In theory will redirect to home page if the user is logged in and will open login page if the user is not
+    {
+      console.log("not logged in");
+      this.authService.login()
+    }
+    else
+    {
+      console.log("logged in");
+      this.router.navigate(['/choose-desk']);
+    }
+  }
+}
