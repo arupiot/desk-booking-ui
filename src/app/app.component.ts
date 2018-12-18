cf4563b11a8616784b23aaff68114f9323648c84
Rory Webber <Rory.Webber@arup.com>
2018-12-18 11:07:37 +0000

    #4 Added a logout button to the top bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
+  constructor(
+    private authService: AuthService
+  ) {}
   title = 'app';
+  logout()
+  {
+    this.authService.logout();
+  }
 }
