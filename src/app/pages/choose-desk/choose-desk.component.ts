5267b3e5a51be7260093816c991d1ee7fd412341
Rory Webber <Rory.Webber@arup.com>
2019-01-24 10:18:11 +0000

    #10 the names of the desk are now from the randomly generated codepen code

--- src/app/pages/choose-desk/choose-desk.component.ts
+++ src/app/pages/choose-desk/choose-desk.component.ts
@@ -8,14 +8,14 @@ import { AuthService } from './../../auth/auth.service';
   styleUrls: ['./choose-desk.component.less']
 })
 export class ChooseDeskComponent implements OnInit {
-  numbers;
+  names;
   constructor(
     
     // public authService: AuthService
   ) { }
 
   ngOnInit() {
-    this.numbers = [1,2,3,4,5,6,7,8,9,10];
+    this.names = ["jovial_goldberg","big_volhard","backstabbing_elion","sick_poincare","mad_carson","furious_payne","cranky_lumiere","hopeful_allen","gloomy_volhard","thirsty_morse"];
   }
 
 }
