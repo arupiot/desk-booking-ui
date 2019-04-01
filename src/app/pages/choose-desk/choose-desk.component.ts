c0d696063a80c5d9be99401f585d721391973f9b
Rory Webber <Rory.Webber@arup.com>
2019-04-01 16:22:48 +0100

    #32 moved the code that was in the init to a different function so that it can be called after a desk has been booked so that the booking appears without the user having to refresh the page

--- src/app/pages/choose-desk/choose-desk.component.ts
+++ src/app/pages/choose-desk/choose-desk.component.ts
@@ -6,6 +6,7 @@ import { ImageMapCoordinate } from './image-map/image-map.component';
 import { ToastrService } from 'ngx-toastr';
 import { MatDialog } from '@angular/material';
 import { BookingModalComponent } from './booking-modal/booking-modal.component';
+import { getViewData } from '@angular/core/src/render3/instructions';
 
 
 @Component({
@@ -46,7 +47,10 @@ export class ChooseDeskComponent implements OnInit {
   coordinate04: ImageMapCoordinate[] = []
 
   ngOnInit() {
-  
+    this.getData();
+    
+  }
+  getData() {
     this.datastoreService.getDesks()
     .subscribe(
       (desks: any) =>{
@@ -112,6 +116,8 @@ export class ChooseDeskComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
+      this.getData();
+
       // this.animal = result;
     });
   }
